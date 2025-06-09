6d36846ee760856320f59bf601c65fe0be377063
admin rrispat <software@rrispat.com>
2025-06-09 11:17:13 +0530

    http -> https

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,7 +1,7 @@
 // core/constants/api_endpoints.dart
 //const String baseUrl = 'http://192.168.13.239:8000/api';
-const String baseUrl = 'http://192.168.13.74:8000/api';
-// const String baseUrl = 'https://beta.rrispat.in/api';
+// const String baseUrl = 'http://192.168.13.74:8000/api';
+const String baseUrl = 'https://beta.rrispat.in/api';
 const String signupUrl = '$baseUrl/auth/signup';
 const String loginUrl = '$baseUrl/auth/login';
 const String meUrl = '$baseUrl/auth/me';
